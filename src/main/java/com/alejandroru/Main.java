e30a4a05fe8dbe817b5bfca47d5bd1addef23797
PrayerMantis <alejandronikiperovich@gmail.com>
2024-03-19 18:34:12 +0100

    Finished "Getting started" section of the course

---
+++ src/main/java/com/alejandroru/Main.java
@@ -0,0 +1,59 @@
+package com.alejandroru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/greet")
+    public GreetResponse greet() {
+        return new GreetResponse(
+                "Hello",
+                List.of("Java", "Golang", "JavaScript"),
+                new Person("Alex", 28, 30000.25)
+        );
+    }
+
+    record Person(String name, int age, double savings){}
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){}
+
+    /* class GreetResponse {
+        private final String greet;
+
+        GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+    } */
+}
